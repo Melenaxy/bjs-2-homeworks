f1a2359672090767cefd5b2a1d7d50610ff6f6e6
MelnikovaEYu <MelnikovaEYu@ufa.infteh.ru>
2023-12-04 00:44:27 +0500

    Homework № 3, 4 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])) {
+        return true;
+    }
+    return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let newArr = users.filter((user) => user.gender === gender).map((item) => item.age)
+    if (newArr.length === 0) return 0;
+    let result = newArr.reduce((sum, current) => sum + current, 0) / newArr.length;
+    return result;
 }
